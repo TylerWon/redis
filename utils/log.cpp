730b2de20e04c5bc9574e0b5a90a8640819be93d
Tyler Won <won.tyler1@gmail.com>
2025-11-20 19:57:36 -0800

    Add timestamp to logs

--- utils/log.cpp
+++ utils/log.cpp
@@ -1,9 +1,18 @@
 #include <cstdarg>
 #include <cstdio>
 #include <cstdlib>
+#include <ctime>
+#include <ostream>
+#include <cstring>
 
 /* Helper that takes a va_list */
 void vlog(const char* fmt, va_list args) {
+    time_t timestamp = time(NULL);
+    char *str = ctime(&timestamp);
+    size_t len = strlen(str);
+    str[len - 1] = '\0'; // remove newline from end of timestamp
+    printf("[%s] ", str);
+
     vfprintf(stdout, fmt, args);
     fprintf(stdout, "\n");
 }
